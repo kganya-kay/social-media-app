e188cb1e118c23244ecca971fbcf012dec5912f3
Doctor Kekana <kganyakekana@gmail.com>
2020-03-13 15:04:43 +0200

    Add files via upload

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the body of the scream',
+            createdAt: "2020-01-19T17:16:52.416Z",
+            likeCount: 5,
+            commentComment: 2
+        }
+    ]
+}
